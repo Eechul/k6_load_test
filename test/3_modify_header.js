1613886e8c41eaadc4db6ac37f079400e4d3674d
이동철 <choise154@gmail.com>
2024-05-11 12:31:18 +0900

    modify_header url 추가

--- test/3_modify_header.js
+++ test/3_modify_header.js
@@ -3,6 +3,7 @@ import http from 'k6/http';
 // 헤더를 변경하여 요청할 수 있다.
 // method 또한 바꿔서 요청을 보낼 수 있다. 
 export default function (data) {
+	const url = 'http://test.k6.io';
 	const params = {
 		headers: {
 		  'X-CUSTOME-HEADER': 'TEST',
